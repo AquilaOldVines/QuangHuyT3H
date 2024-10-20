03183440facbedd0d6960a570a6a12a8d5ec3c00
AquilaOldVines <fconline1092003@gmail.com>
2024-10-20 11:31:34 +0700

    Delete ChiTieuCaNhan directory

--- ChiTieuCaNhan/app/src/main/java/com/example/chitieucanhan/model/piechart/DataOfPiechart.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chitieucanhan.model.piechart
-
-data class DataOfPiechart(
-
-    var typePie : Int,
-    var namePie : String,
-    var moneyPie : Float,
-    var ratioPie : Int
-)
